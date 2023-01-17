baff55539a264cdb97eb2f575081731d6373bc6c
Dani Ponce Mendoza <danniscreamer@gmail.com>
2023-01-17 12:24:16 -0600

    se creo un nuevo repo con sus diversas carpetas

---
+++ async/index.js
@@ -0,0 +1,76 @@
+ // Ejercicio 1
+ const promise = new Promise((resolve, reject) => {
+    const res = true;
+    // An asynchronous operation.
+    if (res) {
+      resolve('Resolved!');
+    }
+    else {
+      reject(Error('Error'));
+    }
+  });
+  
+  promise.then((res) => console.log(res), (err) => alert(err)); 
+
+// Ejercicio 2
+  const enMiCumpleaños = (estaJennyEnferma) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (!estaJennyEnferma) {
+          resolve(2);
+        } else {
+          reject(new Error("Estoy triste"));
+        }
+      }, 2000);
+    });
+  };
+  
+  enMiCumpleaños(false)
+    .then((result) => {
+      console.log(`I have ${result} cakes`); // In the console: I have 2 cakes  
+    })
+    .catch((error) => {
+      console.log(error); // Does not run
+    })
+    .finally(() => {
+      console.log("Party"); // Shows in the console no matter what: Party
+    });
+
+
+// ------ Async/await --------
+
+    async function asinc(){
+        const promise = new Promise((resolve,reject)=>{
+          const res = true;
+          
+          if(res){
+            resolve('Resolved!')
+          }else{
+            reject(Error('Error'))
+          }
+        });
+        
+         let result = await promise; 
+         console.log(result)
+        
+      }
+      
+      asinc()
+
+      //second2
+
+      async function cumpleaños(estaJennyEnferma){
+        const promise = new Promise((resolve,reject) =>{
+           setTimeout(() => {
+              if (!estaJennyEnferma) {
+                resolve(console.log('Jenny va a ir a la fiesta'));
+              } else {
+                reject(new Error("Estoy triste"));
+              }
+            }, 2000);
+        });
+        let result = await promise;
+        console.log(result)
+      }
+      
+      cumpleaños()
